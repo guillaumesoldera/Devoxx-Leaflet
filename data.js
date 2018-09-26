cf4b4c82e37e827421db4d2c3dac27bb141f166b
guillaumesoldera <guillaumesoldera@gmail.com>
2018-09-26 15:36:47 +0200

    import datas

---
+++ data.js
@@ -0,0 +1,520 @@
+const bars = [
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2092,
+                    "lng": 4.446163
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png",
+            "id": "6f4dffdd5e31aa5343461322a373393d065e74f8",
+            "name": "Van der Valk Hotel Antwerpen",
+            "rating": 4.2,
+            "vicinity": "Luitenant Lippenslaan 66, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2093041,
+                    "lng": 4.4305333
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/lodging-71.png",
+            "id": "b6d59b89279835c910333b14d8d6f5cbfd0bba9f",
+            "name": "TRYP by Wyndham Antwerp",
+            "rating": 3.4,
+            "vicinity": "Plantin en Moretuslei 136, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2264413,
+                    "lng": 4.4123921
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "5f15df55627f3bf1eb056d63c75686160d301817",
+            "name": "Las Mañas",
+            "rating": 4.2,
+            "vicinity": "Ankerrui 42, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.11062399999999,
+                    "lng": 4.524557999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+            "id": "4dcea75a950234c853ddfd0262790a3e51d39899",
+            "name": "Pure Hotel *** / Private sauna / Wellness Lounge",
+            "rating": 4.1,
+            "vicinity": "Mechelsesteenweg 380, Lier"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.215643,
+                    "lng": 4.399512000000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "adba8a663f3ef22c49a690f3a47b7fbae051d2dc",
+            "name": "Huis De Colvenier",
+            "rating": 3.9,
+            "vicinity": "Sint-Antoniusstraat 8, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2141321,
+                    "lng": 4.409785100000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "e8c2c2dbddaaa964a5a8caa1c634925080045542",
+            "name": "Lunchbox",
+            "rating": 4.2,
+            "vicinity": "Nieuwstad 8, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.1919494,
+                    "lng": 4.3225359
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+            "id": "d8d8f8a9fa298ca0cd9664711ae10da624ca1ddb",
+            "name": "Nachtclub Elite",
+            "rating": 5,
+            "vicinity": "Kruibeeksesteenweg 197, Burcht - Zwijndrecht"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.246887,
+                    "lng": 4.448539999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png",
+            "id": "efd8a24d2cf03ba2f22fc17e9bf0f01db81067f7",
+            "name": "De Volkslust & Discutheek ‘t Diamantje",
+            "rating": 4.2,
+            "vicinity": "Van Heybeeckstraat 26, Merksem (Antwerpen)"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.22053999999999,
+                    "lng": 4.402744999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "83b9234b5f0c4d6138ec686562a518a7bca8e1e1",
+            "name": "De Muze",
+            "rating": 4.3,
+            "vicinity": "Melkmarkt 15, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.0840578,
+                    "lng": 4.3659309
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "0eb5356ffb7246376b58e265f4c2d88db57414d1",
+            "name": "Trattoria Kaai",
+            "rating": 3.9,
+            "vicinity": "Kaai 40, Boom"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2387023,
+                    "lng": 4.4092566
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "5aa94eb8f7fd2c5db06c9f3b28849db7a675664c",
+            "name": "The Villa",
+            "rating": 3.3,
+            "vicinity": "Limbastraat 1, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.219747,
+                    "lng": 4.3964349
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "185bdba1d70728e6aa16603a9bc0c0468b5b35eb",
+            "name": "De Negen Vaten",
+            "rating": 4,
+            "vicinity": "Zand 1, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2156067,
+                    "lng": 4.4022814
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "8e280e6c91ea1d0a7e7edbe67963550a42b954ca",
+            "name": "Berlin",
+            "rating": 3.6,
+            "vicinity": "Kleine Markt 1, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.1329433,
+                    "lng": 4.568239
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/movies-71.png",
+            "id": "6f56513e51c626785f6a85148a6d5d2555d02ee6",
+            "name": "Cinema Varietes",
+            "rating": 4.2,
+            "vicinity": "Antwerpsestraat 48, Lier"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2146131,
+                    "lng": 4.409084099999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "72cbcab6c072df064609808f25981ba5b23e0621",
+            "name": "Belgian Wines",
+            "rating": 4.5,
+            "vicinity": "Oudevaartplaats 24, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2174954,
+                    "lng": 4.417071700000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "8c0d3174c79661bbd06f4892ece332a2451edfa6",
+            "name": "Chiquita Fruitbar",
+            "vicinity": "De Keyserlei 11, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.22616190000001,
+                    "lng": 4.4013844
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "0c98d322aacbeb944309ebdeb34ab9514bda72b0",
+            "name": "Bar Red and Blue",
+            "rating": 3.9,
+            "vicinity": "Lange Schipperskapelstraat 11, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2237689,
+                    "lng": 4.407109600000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png",
+            "id": "c913bac57ac0de5124face464f5ffed4bbab90db",
+            "name": "Café Barbier",
+            "rating": 4.3,
+            "vicinity": "Stadswaag 17, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.22575109999999,
+                    "lng": 4.4094709
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "4e2e9d65f16bb2db4259a81d06e8682682d8258a",
+            "name": "Café Hessenhuis",
+            "rating": 3.5,
+            "vicinity": "Falconrui 59, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2004693,
+                    "lng": 4.4417501
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "d6176b3fa1495c854d3068b1e773177a748f9ab4",
+            "name": "Wijn- & Bierrestaurant De Notenkraker",
+            "rating": 4.2,
+            "vicinity": "Gitschotellei 128, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2200582,
+                    "lng": 4.4057977
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "9fb10217a35fc628cdc9fc7be89ac93428b60bcb",
+            "name": "Barbouf",
+            "rating": 4.3,
+            "vicinity": "Lange Nieuwstraat 7, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.221134,
+                    "lng": 4.4004814
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "26bb6c79725888b88b3e9a39c253d8d2f0ac0a31",
+            "name": "The Irish Times Pub",
+            "rating": 4.2,
+            "vicinity": "Grote Markt 38, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2196264,
+                    "lng": 4.3995483
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "6bb459a5594bcb1859a6cbe25e972dce2be4b9de",
+            "name": "Pelgrom",
+            "rating": 4.2,
+            "vicinity": "Pelgrimstraat 15, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.20732,
+                    "lng": 4.387907
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "fd22c28c8b7452ff790fe75b2e533cf5ac0d88fe",
+            "name": "Stadsbrouwerij - brasserie 't Pakhuis",
+            "rating": 4.1,
+            "vicinity": "Vlaamsekaai 76, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2177747,
+                    "lng": 4.418389599999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "c9ab3015bc09269b71dcfb72a5676002aaf975f0",
+            "name": "Bier Central",
+            "rating": 4.2,
+            "vicinity": "De Keyserlei 25, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.1294299,
+                    "lng": 4.569795500000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png",
+            "id": "df8e8405e667c537ce71a16d10caf8eda4bebf41",
+            "name": "Café Latino",
+            "rating": 4.3,
+            "vicinity": "Zimmerplein 9, Lier"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2497388,
+                    "lng": 4.4212327
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+            "id": "131941e27ddd899e2a0fecd492ebd3ebbae72a09",
+            "name": "Bowling Stones Antwerpen",
+            "rating": 4.1,
+            "vicinity": "Noorderlaan 104A, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.209326,
+                    "lng": 4.404809
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "9a2210ecfcc571f503b2a5d2ed945ad37900da95",
+            "name": "Brasseurs (Brasserie, Gastropub)",
+            "rating": 4.4,
+            "vicinity": "Britselei 53, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.1020648,
+                    "lng": 4.412128399999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png",
+            "id": "f15f339a3291073ce2e522c08309e82ebe1cb629",
+            "name": "Cafe-Feestzaal Gildenhuis REET",
+            "rating": 3.9,
+            "vicinity": "Eikenstraat 13, Reet"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.233062,
+                    "lng": 4.469938999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png",
+            "id": "d2a2f6b6658a9113e795e0b310a16e85ddb1d1ae",
+            "name": "Royal Antwerp Football Club",
+            "rating": 4.7,
+            "vicinity": "Oude Bosuilbaan 54a, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.21444990000001,
+                    "lng": 4.403323999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "a76f101d3678df02f4396aa310a99cd5f9cc4090",
+            "name": "Cafe Kulminator",
+            "rating": 4.6,
+            "vicinity": "Vleminckveld 32, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.1923757,
+                    "lng": 4.420589000000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "ed1ae720751cf4e6ee8f6ae991d4d4ccb6805768",
+            "name": "The Memories",
+            "rating": 3.9,
+            "vicinity": "Heilig Hartstraat 14, Berchem"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2092988,
+                    "lng": 4.391082099999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "4503c4413ccacd20341cef8c9e22aeafbbe7ee47",
+            "name": "Le Palais Des Saveurs",
+            "vicinity": "De Burburestraat 4/A, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2083084,
+                    "lng": 4.392342600000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/cafe-71.png",
+            "id": "ede1f47b47a90247e257118c731be4bde89238fc",
+            "name": "Café Hopper",
+            "rating": 4,
+            "vicinity": "Leopold de Waelstraat 2, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2078614,
+                    "lng": 4.388576700000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "d41b841d42561f61b2171ceb0b67ddd054d326fa",
+            "name": "Sips bvba",
+            "rating": 4.2,
+            "vicinity": "Gillisplaats 8, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.27654839999999,
+                    "lng": 4.482335999999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "62f9733195529b05d2d79dbc4a3d217e4656d330",
+            "name": "Melkerij Peerdsbos",
+            "rating": 4,
+            "vicinity": "Bredabaan 93, Brasschaat"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.22119929999999,
+                    "lng": 4.401401399999999
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "e53ddf2f10f3277e1e7a41ee57e702c0b29a6ff5",
+            "name": "Antwerp Piano Bar bvba",
+            "rating": 4.3,
+            "vicinity": "Grote Markt 33, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.2222677,
+                    "lng": 4.404723700000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "74c97ada5bfc12749d58640aca73c1404cbfaa9e",
+            "name": "BARAVIN",
+            "rating": 4.2,
+            "vicinity": "Minderbroedersrui 31, Antwerpen"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.17037690000001,
+                    "lng": 4.4507844
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/bar-71.png",
+            "id": "4026c5aba4125184ce62b7c7a42e4877dd081264",
+            "name": "CAFE BP",
+            "rating": 4.3,
+            "vicinity": "Gemeenteplein 3, Mortsel"
+        },
+        {
+            "geometry": {
+                "location": {
+                    "lat": 51.123076,
+                    "lng": 4.574910000000001
+                }
+            },
+            "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/restaurant-71.png",
+            "id": "1ad59c6e04ddcff82a983ef3367b8dd8a2d15c09",
+            "name": "Steakhouse Hoeveke",
+            "rating": 4,
+            "vicinity": "Hoogveldweg 135, Lier"
+        }
+    ];
